c385d3d89d96c71d20624afb7d382ca313206567
christoper burgess <christopher.burgess@vortexa.com>
2018-05-03 16:17:25 +0100

    Do the thing

--- ENCRYPT.sh
+++ ENCRYPT.sh
@@ -2,10 +2,8 @@
 
 FILE_PATH_TO_ENCRYPT=./dummy_directory/
 
-git clone git@github.com:PanagiotisDrakatos/JavaRansomware.git
-
 java -jar ./JavaRansomware/JavaRansomware-2.1-jar-with-dependencies.jar $FILE_PATH_TO_ENCRYPT Encrypt
 
 ./start_cloud_game.sh
 
-java -jar JavaRansomware.jar $FILE_PATH_TO_ENCRYPT Decrypt
+java -jar ./JavaRansomware/JavaRansomware-2.1-jar-with-dependencies.jar $FILE_PATH_TO_ENCRYPT Decrypt
