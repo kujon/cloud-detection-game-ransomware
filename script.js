6d0f3443d9d5c0a7fca1e645aa3ea80debb15a6a
kujon <jakub.korzeniowski@gmail.com>
2018-05-03 17:10:40 +0100

    kick ass ui

--- script.js
+++ script.js
@@ -3,51 +3,82 @@ const drawCurrentImage = ctx => {
     ctx.drawImage(img, 0, 0);
 };
 
-const drawLine = (ctx, coordinates) => {
-    for (let i = 0; i < coordinates.length; i++) {
-        if (i === 0) {
-            ctx.beginPath();
-            ctx.moveTo(coordinates[i][0], coordinates[i][1]);
-        } else {
-            ctx.lineTo(coordinates[i][0], coordinates[i][1]);
-            ctx.stroke();
+const drawLine = (ctx, coordinatesSet) => {
+    for (const coordinates of coordinatesSet) {
+        for (let i = 0; i < coordinates.length; i++) {
+            if (i === 0) {
+                ctx.beginPath();
+                ctx.moveTo(coordinates[i][0], coordinates[i][1]);
+            } else {
+                ctx.lineTo(coordinates[i][0], coordinates[i][1]);
+                ctx.stroke();
+            }
         }
     }
 };
 
-const finalise = coordinates => {
-
+const redrawCanvas = (ctx, coordinates) => {
+    ctx.clearRect(0, 0, 660, 371);
+    drawCurrentImage(ctx);
+    drawLine(ctx, coordinates);
 };
 
-window.addEventListener('load', () => {
+const finalise = coordinates => {};
+
+const handleImage = () => {
     const c = document.getElementById("canvas");
     const ctx = c.getContext("2d");
 
-    drawCurrentImage(ctx);
+    ctx.strokeStyle = "white";
+    ctx.lineWidth = 4;
+
+    redrawCanvas(ctx, []);
 
     let coordinates = [];
     let clickCount = 0;
 
-    c.addEventListener('mousedown', e => {
+    const mouseMove = e => {
+        const x = e.layerX;
+        const y = e.layerY;
+        redrawCanvas(
+            ctx,
+            coordinates
+                .slice(0, coordinates.length - 1)
+                .concat([coordinates[coordinates.length - 1].concat([[x, y]])])
+        );
+    };
+
+    c.addEventListener("mousedown", e => {
         const x = e.layerX;
         const y = e.layerY;
 
-        coordinates.push([x, y]);
+        if (clickCount === 0) {
+            coordinates[coordinates.length] = [];
+        }
+
+        coordinates[coordinates.length - 1].push([x, y]);
 
         clickCount = clickCount + 1;
 
         if (clickCount === 1) {
-            c.addEventListener('mousemove', e => {
-                drawLine(ctx, coordinates);
-            });
+            c.addEventListener("mousemove", mouseMove);
         }
-
-        if (clickCount > 4) {
-            const finalCoordinates = coordinates.slice(0, 4).concat([coordinates[0]]);
-            drawLine(ctx, finalCoordinates);
-            finalise(finalCoordinates);
-            coordinates = [];
+        // console.log(coordinates);
+        if (clickCount === 4) {
+            coordinates = coordinates
+                .slice(0, coordinates.length - 1)
+                .concat([
+                    coordinates[coordinates.length - 1].concat([
+                        coordinates[coordinates.length - 1][0]
+                    ])
+                ]);
+            redrawCanvas(ctx, coordinates);
+            c.removeEventListener("mousemove", mouseMove);
             clickCount = 0;
         }
     });
+};
+
+window.addEventListener("load", () => {
+    handleImage();
 });
